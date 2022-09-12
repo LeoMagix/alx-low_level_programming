464c454f8a13bb2dcbb09966c756916e6f1cde86
LeoMagix <dahleo08@gmail.com>
2022-09-12 13:30:41 -0700

    use puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - will print the string from the main function
+ *
+ * Description - Prints the statements using puts
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
