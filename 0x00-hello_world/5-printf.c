6cc1ecdf8ca0d12b5873d07327155963f003c986
LeoMagix <dahleo08@gmail.com>
2022-09-12 13:57:37 -0700

    print with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the script with the printf function
+ *
+ * Description - uses the main function
+ * This programs pirnts the statement "with proper grammar,
+ * but the outcome is a piece of art"
+ * Return: 0
+ */
+int main(void)
+
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
