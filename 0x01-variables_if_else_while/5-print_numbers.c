5cd926fb9fbbd03d8afb53d8fd235160250a0d27
LeoMagix <dahleo08@gmail.com>
2022-09-17 15:51:16 -0700

    print base10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print tme numbers in base 10
+ *
+ * Description - using main function
+ * program "primst the numbers in base10 format"
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	printf("%i\n", n);
+
+	return (0);
+}
