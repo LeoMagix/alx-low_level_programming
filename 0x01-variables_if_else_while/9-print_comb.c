e3747a7186d91e06f95e41ddf0b50eb246c042e6
LeoMagix <dahleo08@gmail.com>
2022-09-18 11:02:29 -0700

    print single digit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print single digit combinations
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+
+			if (i != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+	}
+
+	putchar('\n');
+	return (0);
+}
