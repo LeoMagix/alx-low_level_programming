55201000165cfa32adac85b35ec5607fd283f700
LeoMagix <dahleo08@gmail.com>
2022-09-26 13:33:15 -0700

    get 1-14 10times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	char a, c;
+	int n = 0;
+
+	while (n < 10)
+	{
+		for (a = 0; a <= 14; a++)
+		{
+			c = a;
+			if (a > 9)
+			{
+				_putchar('1');
+				c = a % 10;
+			}
+			_putchar('0' + c);
+		}
+
+		_putchar('\n');
+		n++;
+	}
+}
