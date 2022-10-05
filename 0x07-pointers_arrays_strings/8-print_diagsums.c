8ede68d9f8d3e70fb9e67131610863fe3a229a6b
LeoMagix <dahleo08@gmail.com>
2022-10-05 13:54:20 -0700

    Always square ur matrix

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - sum of two diagonals of
+ * a square of matrix integers
+ *
+ * @a: matrix integer
+ * @size: size pf the matrix
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, sum1 = 0, sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += a[i];
+		a += size;
+	}
+
+	a -= size;
+
+	for (i = 0; i < size; i++)
+	{
+		sum2 += a[i];
+		a -= size;
+	}
+
+	printf("%d, %d\n", sum1, sum2);
+}
