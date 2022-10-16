8606b659e354fbddc744f30c8a894083697c592e
LeoMagix <dahleo08@gmail.com>
2022-10-16 03:36:15 -0700

    Absolute of numbers

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,7 @@
 #ifndef FUNCTION_LIKE_H
 #define FUNCTION_LIKE_H
 
-/**
+/*
  * File: 3-function_like_macro.h
  * Desc: Header file that defines a function-like macro ABS(x)
  *  that computes the absolute value of a number x.
