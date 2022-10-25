28333fdef53eab0773f9a256c9eceb218996a2a5
LeoMagix <dahleo08@gmail.com>
2022-10-25 11:17:58 -0700

    compulsory files

---
+++ 0x13-more_singly_linked_lists/6-main.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - Check the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	listint_t *head;
+	int n;
+
+	head = NULL;
+	add_nodeint(&head, 0);
+	add_nodeint(&head, 1);
+	add_nodeint(&head, 2);
+	add_nodeint(&head, 3);
+	add_nodeint(&head, 4);
+	add_nodeint(&head, 98);
+	add_nodeint(&head, 402);
+	add_nodeint(&head, 1024);
+	print_listint(head);
+	n = pop_listint(&head);
+	printf("- %d\n", n);
+	print_listint(head);
+	n = pop_listint(&head);
+	printf("- %d\n", n);
+	print_listint(head);
+	free_listint2(&head);
+	printf("%p\n", (void *)head);
+	return (0);
+}
